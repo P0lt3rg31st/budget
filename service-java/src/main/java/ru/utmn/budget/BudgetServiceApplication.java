7b7fb369c646997fad99a99e7395e3a277ed298d
Petrichor <vikachris2007@gmail.com>
2026-03-05 18:42:06 +0300

    Initial commit

---
+++ service-java/src/main/java/ru/utmn/budget/BudgetServiceApplication.java
@@ -0,0 +1,8 @@
+package ru.utmn.budget;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetServiceApplication {
+    public static void main(String[] args) {}
+}
