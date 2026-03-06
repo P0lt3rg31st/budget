2572071c22e532a7d1a6a0c091211935a6cdfc2f
Petrichor <vikachris2007@gmail.com>
2026-03-06 15:37:05 +0300

    checkstyle

--- service-java/src/main/java/ru/utmn/budget/BudgetServiceApplication.java
+++ service-java/src/main/java/ru/utmn/budget/BudgetServiceApplication.java
@@ -4,5 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BudgetServiceApplication {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+    }
 }
